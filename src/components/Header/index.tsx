4b009affcf3ed2bc9008e303c9fe3a138bb6c7fa
Marlon Tereziano <80834228+MarlonTereziano@users.noreply.github.com>
2022-08-10 19:11:08 -0300

    fix: identation Header code

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,75 +1,68 @@
 //Styles
 import {
-    HeaderContainer,
-    InputContainer,
-    LoginContainer,
-    UtilsContainer
-} from './styles';
+  HeaderContainer,
+  InputContainer,
+  LoginContainer,
+  UtilsContainer,
+} from "./styles";
 
-//Next Imports 
-import Image from 'next/image';
-import Link from 'next/link'
+//Next Imports
+import Image from "next/image";
+import Link from "next/link";
 
 //Assets
-import LogoKabum from '../../../public/assets/LogoKabum.svg';
-import InputStyle from '../../../public/assets/InputStyle.svg';
-import ProfileIcon from '../../../public/assets/ProfileIcon.svg';
-import SacIcon from '../../../public/assets/SacIcon.svg';
-import FavoriteIcon from '../../../public/assets/FavoriteIcon.svg';
-import ShopIcon from '../../../public/assets/ShopIcon.svg';
-import KabumMiniLogo from '../../../public/assets/KabumMiniLogo.svg';
+import LogoKabum from "../../../public/assets/LogoKabum.svg";
+import InputStyle from "../../../public/assets/InputStyle.svg";
+import ProfileIcon from "../../../public/assets/ProfileIcon.svg";
+import SacIcon from "../../../public/assets/SacIcon.svg";
+import FavoriteIcon from "../../../public/assets/FavoriteIcon.svg";
+import ShopIcon from "../../../public/assets/ShopIcon.svg";
+import KabumMiniLogo from "../../../public/assets/KabumMiniLogo.svg";
 
 export function Header() {
-    return (
-        <HeaderContainer>
-            <a className="logoNormal" href="/">
-                <Image
-                    src={LogoKabum}
-                    alt="LogoKabum"
-                />
-            </a>
-            <a className="logoMini" href="/">
-                <Image
-                    src={KabumMiniLogo}
-                    alt="LogoKabum"
-                />
-            </a>
-            <InputContainer>
-                <input type="text" placeholder='Busque aqui' />
-                <div className="contentSearch">
-                    <Image src={InputStyle} alt="input Search" width={500} />
-                    <button />
-                </div>
-            </InputContainer>
-            <LoginContainer>
-                <div className="contentLogin">
-                    <Image src={ProfileIcon} alt="ProfileIcon" />
-                    <span>
-                        Faça
-                        <Link href="/">
-                            <a>Login </a>
-                        </Link>
-                        ou
-                        <br />
-                        crie seu
-                        <Link href="/">
-                            <a>Cadastro</a>
-                        </Link>
-                    </span>
-                </div>
-            </LoginContainer>
-            <UtilsContainer>
-                <a target="_blank" href='https://www.kabum.com.br/faq'>
-                    <Image src={SacIcon} alt="SacIcon" />
-                </a>
-                <a target="_blank" href='/'>
-                    <Image src={FavoriteIcon} alt="FavoriteIcon" />
-                </a>
-                <a target="_blank" href="https://www.kabum.com.br/carrinho">
-                    <Image src={ShopIcon} alt="ShopIcon" />
-                </a>
-            </UtilsContainer>
-
-        </HeaderContainer>
-    );
+  return (
+    <HeaderContainer>
+      <a className="logoNormal" href="/">
+        <Image src={LogoKabum} alt="LogoKabum" />
+      </a>
+      <a className="logoMini" href="/">
+        <Image src={KabumMiniLogo} alt="LogoKabum" />
+      </a>
+      <InputContainer>
+        <input type="text" placeholder="Busque aqui" />
+        <div className="contentSearch">
+          <Image src={InputStyle} alt="input Search" width={500} />
+          <button />
+        </div>
+      </InputContainer>
+      <LoginContainer>
+        <div className="contentLogin">
+          <Image src={ProfileIcon} alt="ProfileIcon" />
+          <span>
+            Faça
+            <Link href="/">
+              <a>Login </a>
+            </Link>
+            ou
+            <br />
+            crie seu
+            <Link href="/">
+              <a>Cadastro</a>
+            </Link>
+          </span>
+        </div>
+      </LoginContainer>
+      <UtilsContainer>
+        <a target="_blank" href="https://www.kabum.com.br/faq">
+          <Image src={SacIcon} alt="SacIcon" />
+        </a>
+        <a target="_blank" href="/">
+          <Image src={FavoriteIcon} alt="FavoriteIcon" />
+        </a>
+        <a target="_blank" href="https://www.kabum.com.br/carrinho">
+          <Image src={ShopIcon} alt="ShopIcon" />
+        </a>
+      </UtilsContainer>
+    </HeaderContainer>
+  );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,168 +1,168 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    background-color:var(--color-blue);
-    width: 100vw;
-    max-width: 1440px;
-    overflow: hidden;
-    height: 6.5rem; 
-    padding-left: 3rem;
-    padding-right: 3rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background-color: var(--color-blue);
+  width: 100vw;
+  max-width: 1440px;
+  overflow: hidden;
+  height: 6.5rem;
+  padding-left: 3rem;
+  padding-right: 3rem;
 
-    @media (max-width: 1279px) {
-        .logoNormal{
-            display: none;
-        }
+  @media (max-width: 1279px) {
+    .logoNormal {
+      display: none;
     }
-    @media (min-width: 1280px) {
-        .logoMini{
-            display: none;
-        }
+  }
+  @media (min-width: 1280px) {
+    .logoMini {
+      display: none;
     }
-    @media (max-width: 967px) {
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: auto;
-    }
-`
+  }
+  @media (max-width: 967px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: auto;
+  }
+`;
 
 export const InputContainer = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    margin-left: 5rem;
-    width: 37%;
-    height: 3rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-left: 5rem;
+  width: 37%;
+  height: 3rem;
 
-    input{
-        font-size: 14px;
-        margin-left: 4%;
-        border-radius: 4px;
-        border: none;
-        width:25rem;
-        height: 1.7rem;
-        z-index: 1;
-        background-color: transparent;
-    }
+  input {
+    font-size: 14px;
+    margin-left: 4%;
+    border-radius: 4px;
+    border: none;
+    width: 25rem;
+    height: 1.7rem;
+    z-index: 1;
+    background-color: transparent;
+  }
 
-    .contentSearch{
-        flex-direction: row;
-        position: absolute;
-        width: 50%;
-        justify-content: center;
-        align-items: center;
-    }
-
-    button{
-        position: relative;
-        margin-left: -8%;
-        width: 8%;
-        height: 3rem;
-        z-index: 2;
-        background-color: transparent;
-        border: none;
-    }
-
-    button:hover{
-        cursor:pointer;
-    }
-
-    @media (max-width: 1440px) {
-        margin-right: 10%;
-    }
-    @media (max-width: 1150px) {
-        width: 45%;
-        margin-left: 0;
-
-        input{
-            height: 1.5rem;
-            background-color: transparent;
-        }
-
-        .contentSearch{
-            width: 30rem;
-        }
-    }
-    @media (max-width: 967px) {
-        margin-left: -10%;
-        width: 20rem;
-        .contentSearch{
-            width: 30rem;
-            margin: 0;
-        }
-    }
-    @media (max-width: 580px) {
-        margin-left: 10%;
-        input{
-            margin-top:-0.5rem;
-            height: 1rem;
-        }
-        .contentSearch{
-            margin-top: -5%;
-            width: 20rem;
-        }
-    }
-`
-export const LoginContainer = styled.div`
-    display: flex;
+  .contentSearch {
+    flex-direction: row;
+    position: absolute;
+    width: 50%;
     justify-content: center;
     align-items: center;
-    margin-right: -3rem;
-    width: 15%;
-    height: 50%; 
+  }
 
-    .contentLogin{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-size: 12px;
-        color:var(--color-white-200);
+  button {
+    position: relative;
+    margin-left: -8%;
+    width: 8%;
+    height: 3rem;
+    z-index: 2;
+    background-color: transparent;
+    border: none;
+  }
 
-        span{
-            margin-left: 0.5rem;    
-            z-index: 1;
-        }
+  button:hover {
+    cursor: pointer;
+  }
 
-        a{  
-            margin-left: 4px;
-            font-weight: bold;
-            color:var(--color-white-200);
-        }
+  @media (max-width: 1440px) {
+    margin-right: 10%;
+  }
+  @media (max-width: 1150px) {
+    width: 45%;
+    margin-left: 0;
 
-        a:hover{
-            text-decoration: underline;
-        }
+    input {
+      height: 1.5rem;
+      background-color: transparent;
     }
 
-    @media (max-width: 1150px) {
-        width: 25%;
-        margin-right: -20%;
+    .contentSearch {
+      width: 30rem;
     }
-    @media (max-width: 967px) {
-        width: 15rem;
-        margin-right: 0;
+  }
+  @media (max-width: 967px) {
+    margin-left: -10%;
+    width: 20rem;
+    .contentSearch {
+      width: 30rem;
+      margin: 0;
     }
-`
+  }
+  @media (max-width: 580px) {
+    margin-left: 10%;
+    input {
+      margin-top: -0.5rem;
+      height: 1rem;
+    }
+    .contentSearch {
+      margin-top: -5%;
+      width: 20rem;
+    }
+  }
+`;
+export const LoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: -3rem;
+  width: 15%;
+  height: 50%;
+
+  .contentLogin {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 12px;
+    color: var(--color-white-200);
+
+    span {
+      margin-left: 0.5rem;
+      z-index: 1;
+    }
+
+    a {
+      margin-left: 4px;
+      font-weight: bold;
+      color: var(--color-white-200);
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
+  }
+
+  @media (max-width: 1150px) {
+    width: 25%;
+    margin-right: -20%;
+  }
+  @media (max-width: 967px) {
+    width: 15rem;
+    margin-right: 0;
+  }
+`;
 
 export const UtilsContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-left: 10rem;
-    width: 15%;
-    height: 50%; 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 10rem;
+  width: 15%;
+  height: 50%;
 
-    a{
-        margin: 0.5rem;
-    }
+  a {
+    margin: 0.5rem;
+  }
 
-    @media (max-width: 967px) {
-        margin: 0;
-        width: 10rem;
-    }
-`
+  @media (max-width: 967px) {
+    margin: 0;
+    width: 10rem;
+  }
+`;
