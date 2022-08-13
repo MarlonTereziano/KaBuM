229f165c2f862dcc74c838ca13335d99ec04c16f
Marlon Tereziano <80834228+MarlonTereziano@users.noreply.github.com>
2022-08-13 03:39:47 -0300

    style: banner visibility

--- src/styles/homePage.ts
+++ src/styles/homePage.ts
@@ -22,13 +22,19 @@ export const Content = styled.div`
   flex-direction: column;
   max-width: 1440px;
   background-color: var(--color-white-300);
-  margin-top: -10vw;
+  margin-top: -15rem;
   width: 100vw;
   height: 55.813rem;
   z-index: 3;
 
-  @media (max-width: 967px) {
+  @media (max-width: 1150px) {
     height: 20rem;
+    margin-top: -10rem;
+  }
+  
+  @media (max-width: 750px) {
+    height: 20rem;
+    margin-top: -4rem;
   }
 
   @media (min-width: 2200px) {
